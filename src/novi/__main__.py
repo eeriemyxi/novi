b21b7bc5690317d8b8d9962bf06e82c1779d0991
myxi <myxi@envs.net>
2024-03-17 11:37:33 +0530

    fix: manually redirect URLs but with checks to determine incorrect words

--- src/novi/__main__.py
+++ src/novi/__main__.py
@@ -80,7 +80,6 @@ def handle_word(console: rich.console.Console, client: httpx.Client, word: str) 
 
 def main() -> tuple[int, httpx.Client]:
     client = httpx.Client(
-        follow_redirects=True,
         base_url=constants.BASE_URL,
         headers={"user-agent": constants.USER_AGENT},
     )
--- src/novi/http.py
+++ src/novi/http.py
@@ -2,17 +2,31 @@ import logging
 
 import httpx
 
-from novi.struct import SupportedLanguage
+from novi import constants, struct
 
 log = logging.getLogger(__name__)
 
 
 def get_word_file(
-    client: httpx.Client, language: SupportedLanguage, word: str
+    client: httpx.Client, language: struct.SupportedLanguage, word: str
 ) -> str | None:
-    resp = client.get(f"dictionary/{language.value}/{word}")
+    base_url = f"{constants.BASE_URL}/dictionary/{language.value}"
+    log.debug(f"{base_url=}")
+    resp = client.get(f"{base_url}/{word}")
     log.debug(f"{resp=}")
-    if resp.status_code != 200:
-        log.debug("Could not find word file for %s.", repr(word))
-        return None
-    return resp.content.decode()
+
+    if resp.status_code == 302:
+        location = resp.headers.get("location")
+        log.debug(f"{location=}")
+
+        if location == base_url + "/":
+            log.debug("Could not find word file for %s.", repr(word))
+            return None
+
+        log.debug(f"Redirecting to {location=}")
+        resp = client.get(location)
+
+    if resp.status_code == 200:
+        return resp.content.decode()
+
+    return None
